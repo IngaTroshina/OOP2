04ff94193efef1cd667cb9eceb64ffc75ee2f00e
I.Troshina <inga-troshina@mail.ru>
2023-03-16 17:49:20 +0300

    HW done

---
+++ src/MotorVehicle.java
@@ -0,0 +1,14 @@
+public abstract class MotorVehicle extends Vehicle implements Diagnosable {
+    public MotorVehicle(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+
+    @Override
+    public void service() {
+        super.service();
+        checkEngine();
+    }
+}
